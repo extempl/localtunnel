6aa63c1712a4e4e6140eefe0f6d648413218d4ec
extempl <extempl@gmail.com>
2016-05-24 08:46:12 +0300

    import old duplex

--- server.go
+++ server.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/inconshreveable/go-vhost"
-	"github.com/progrium/duplex/poc2/duplex"
+	"github.com/progrium/duplex/tree/old-2015/poc2/duplex"
 )
 
 func server(backendListen, frontendListen string) {
